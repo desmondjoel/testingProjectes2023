92a19a355119ca20332cd59f038f1c8d27e53e8f
desmondjoel <joeldesmond4@gmail.com>
2023-10-09 10:52:04 +0530

    table1

---
+++ Oct_09/Lab002.py
@@ -0,0 +1,31 @@
+a = input("enter the number\n")
+
+a1=int(a)
+
+ans1=a1 * 1
+
+ans2=a1 * 2
+
+ans3 = a1 * 3
+
+ans4 = a1 * 4
+ans5 = a1 * 5
+ans6 = a1 * 6
+ans7 = a1 * 7
+ans8 = a1 * 8
+ans9 = a1 * 9
+ans10 = a1 * 10
+
+
+
+print(f"{a1} {'* 1 ='} {ans1}\n"
+      f"{a1} {'* 2 ='} {ans2}\n"
+      f"{a1} {'* 3 ='} {ans3}\n"
+      f"{a1} {'* 4 ='} {ans4}\n" 
+f"{a1} {'* 5 ='} {ans5}\n" 
+f"{a1} {'* 6 ='} {ans6}\n"
+f"{a1} {'* 7 ='} {ans7}\n"
+f"{a1} {'* 8 ='} {ans8}\n"
+f"{a1} {'* 9 ='} {ans9}\n"
+      f""f"{a1} {'* 10 ='} {ans10}\n")
+
