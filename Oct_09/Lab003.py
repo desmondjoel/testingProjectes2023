1febc70f3e01e26e7bf72ab0d0c676269e1ffb10
desmondjoel <joeldesmond4@gmail.com>
2023-10-09 11:42:52 +0530

    projects

---
+++ Oct_09/Lab003.py
@@ -0,0 +1,15 @@
+name ="desmond Joel"
+name1="dheeraj"
+print(name.capitalize())
+print(name1.capitalize())
+
+#-------------------------------------------
+
+print(id(name))
+print(id(name1))
+
+print(name.upper())
+print(name.lower())
+print(name.swapcase())
+
+print(len(name1))
---
+++ Oct_09/Lab004.py
@@ -0,0 +1,9 @@
+text= "Hello World"
+index=text.find("World")
+print(index)
+
+count=text.count("l")
+print(count)
+
+name="Joel"
+print(name)
